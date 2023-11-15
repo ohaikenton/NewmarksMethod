511649cb0bc9b285748f2df06fb638332f0bc414
Kenton Li <kentonli622@gmail.com>
2023-11-16 00:48:30 +0800

    Put parameters inside function

--- main.m
+++ main.m
@@ -1,7 +1,31 @@
-function [time, displacements, velocities, accelerations] = newmarks_method(M, C, K, Phi, u0, v0, dt, gamma, beta, timesteps, u_g0)
+function [time, displacements, velocities, accelerations] = newmarks_method()
     % Step 1.0: Initial calculations
-    q = u0;
-    q_dot = v0;
+
+    Phi = [0.3338, -0.8953, 1.1726, 1.0776, -0.6405;
+       0.6405, -1.1726, 0.3338, -0.8953, 1.0776;
+       0.8953, -0.6405, -1.0776, -0.3338, -1.1726;
+       1.0776, 0.3338, -0.6405, 1.1726, 0.8953;
+       1.1726, 1.0776, 0.8953, -0.6405, -0.3338];
+    gamma = 0.5;
+    beta = 1/6;
+    dt = 0.1;
+    K = [31.3, 0, 0, 0, 0;
+         0, 266.4, 0, 0, 0;
+         0, 0, 0, 662, 0;
+         0, 0, 0, 1092.6, 0;
+         0, 0, 0, 0, 1421.3];
+    C = [0.5592, 0, 0, 0, 0;
+         0, 1.6322, 0, 0, 0;
+         0, 0, 2.573, 0, 0;
+         0, 0, 0, 3.3054, 0;
+         0, 0, 0, 0, 3.77];
+    M = eye(5);
+    p = zeros(5, 1);
+    timesteps = 22;
+    u_g0 = 193;
+
+    q = zeros(5, 1);
+    q_dot = zeros(5, 1);
     q_double_dot = (M \ zeros(5,1)) - C*q_dot - K*q;
 
     % Step 1.5: Define coefficients
@@ -52,28 +76,3 @@ function [time, displacements, velocities, accelerations] = newmarks_method(M, C
         accelerations(i, :) = q_double_dot';
     end
 end
-
-Phi = [0.3338, -0.8953, 1.1726, 1.0776, -0.6405;
-       0.6405, -1.1726, 0.3338, -0.8953, 1.0776;
-       0.8953, -0.6405, -1.0776, -0.3338, -1.1726;
-       1.0776, 0.3338, -0.6405, 1.1726, 0.8953;
-       1.1726, 1.0776, 0.8953, -0.6405, -0.3338];
-gamma = 0.5;
-beta = 1/6;
-dt = 0.1;
-K = [31.3, 0, 0, 0, 0;
-     0, 266.4, 0, 0, 0;
-     0, 0, 0, 662, 0;
-     0, 0, 0, 1092.6, 0;
-     0, 0, 0, 0, 1421.3];
-C = [0.5592, 0, 0, 0, 0;
-     0, 1.6322, 0, 0, 0;
-     0, 0, 2.573, 0, 0;
-     0, 0, 0, 3.3054, 0;
-     0, 0, 0, 0, 3.77];
-M = eye(5);
-p = zeros(5, 1);
-u0 = zeros(5, 1);
-v0 = zeros(5, 1);
-timesteps = 22;
-u_g0 = 193;
